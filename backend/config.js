5b3fcd8732057979fe06d7713e6344cf6badc63f
Evewwal12 <wxy1012candoit@163.com>
2024-04-09 13:56:24 -0700

    final project

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://xinyiw12:Wxy10127790@cluster0.awqajkp.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+// app.listen(PORT,()=>{
+//   console.log('lol')
+// })
+
+// app.get('/',(request,response)=>{
+//   console.log(request)
+//   return response.status(234).send('hii')
+// });
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+app.use('/books', booksRoute);
+//有了books,booksRoute就不用加book prefix勒
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
